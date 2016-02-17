f7b21e584c735ff5de9bc46501665ca19125ed17
Jordan Meier <jbirdmeier@gmail.com>
2016-02-17 08:50:06 -0800

    add and pass first spec, determining if inputs are the same for a result of draw

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -1,9 +1,13 @@
 <?php
     class RockPaperScissors
     {
-
-
-
+        function playGame($input1, $input2)
+        {
+            if ($input1 == $input2) {
+                $result = "draw";
+            }
+            return $result;
+        }
 
 
     }
--- tests/RockPaperScissorsTest.php
+++ tests/RockPaperScissorsTest.php
@@ -1,16 +1,42 @@
-<!-- testing with php unit, use this template for guidance -->
 <?php
-    require_once __DIR__ . '/../src/RockPaperScissors.php';
+
+    require_once "src/RockPaperScissors.php";
 
     class RockPaperScissorsTest extends PHPUnit_Framework_TestCase
     {
-        function test_methodToTest_inputDescription()
+
+        function test_rockrock_paperpaper_scissorscissor()
         {
-            // Arrange
+            //Arrange
+            $test_RockPaperScissors = new RockPaperScissors;
+            $input1 = "rock";
+            $input2 = "scissors";
+            $input3 = "paper";
 
-            // Act
+            //Act
+            $result1 = $test_RockPaperScissors->playGame($input1, $input1);
+            $result2 = $test_RockPaperScissors->playGame($input2, $input2);
+            $result3 = $test_RockPaperScissors->playGame($input3, $input3);
 
-            // Assert
+            //Assert
+            $this->assertEquals("draw", $result1);
+            $this->assertEquals("draw", $result2);
+            $this->assertEquals("draw", $result3);
         }
+        // function test_rock_scissors()
+        // {
+        //     //Arrange
+        //     $test_RockPaperScissors = new RockPaperScissors;
+        //     $input1 = "rock";
+        //     $input2 = "scissors";
+        //
+        //     //Act
+        //     $result = $test_RockPaperScissors->playGame($input1, $input2);
+        //
+        //     //Assert
+        //     $this->assertEquals("Player 1", $result);
+        // }
+
     }
- ?>
+
+?>
