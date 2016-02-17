42c86cc9b2fa87a87e2a927fd8546fbd51609b24
Jordan Meier <jbirdmeier@gmail.com>
2016-02-17 09:25:21 -0800

    add UI, by adding a home route and a form route to app.php, and content to index.html.twig

--- app/app.php
+++ app/app.php
@@ -1,5 +1,6 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/RockPaperScissors.php";
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -7,8 +8,15 @@
     ));
 
     $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/rock_paper_scissors", function() use ($app) {
+        $my_RockPaperScissors = new RockPaperScissors;
+        $result = $my_RockPaperScissors->playGame($_GET['input_1'], $_GET['input_2']);
+
         return $app['twig']->render('index.html.twig', array(
-            // twig input associative array
+            'results' => $result
         ));
     });
 
